3770ea35cda9fba776ec1214de28eafed644a1e9
Christian Aquino <CAquino.dev@gmail.com>
2025-02-26 22:47:25 +0800

    Design the user navbar

--- src/components/UserNavbar.jsx
+++ src/components/UserNavbar.jsx
@@ -1,11 +1,22 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import DUV from "../assets/DUVLogoWhite.png"
+
 
 const UserNavbar = () => {
   return (
-    <div>
-      <Link to={"/"}>Homepage</Link>
-      <Link to={"/Login"}>Login</Link>
+    <div className='flex fixed top-0 left-0 right-0 bg-[#4c735c] h-[60px] justify-between px-10 mb-[60px]'>
+      <div className='left-section flex items-center font-[900]'>
+        <div className='w-[100px]'>
+          <img src={DUV} alt="" />
+        </div>
+      </div>
+      <div className='right-section flex items-center w-[500px] justify-between text-white font-[700]'>
+        <Link to="projects" >Projects</Link>
+        <Link to="our-team" >Our Team</Link>
+        <Link to="about-us" >About us</Link>
+        <Link to={"/Login"}>Login</Link>
+      </div>
     </div>
   )
 }

425d2292f0238aa109ddd5e4770ba722f96ea971
JasTenC <gorduizjustincarl@gmail.com>
2025-03-01 14:24:36 +0800

    added a admin route

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-d
 import Homepage from './pages/Homepage'
 import Login from './pages/Login'
 import UserLayout from './layouts/UserLayout'
+import AdminLayout from './layouts/AdminLayout'
+import  AdminDashboard  from './pages/admin/AdminDashboard'
 
 const App = () => {
   return (
@@ -13,9 +15,22 @@ const App = () => {
           <Route path='/Login' element={<Login/>} />
         </Route>
       </Routes>
+
+      <Routes>
+        <Route path='/admin' element={<AdminLayout/>} >
+          <Route index element={<AdminDashboard/>} />
+        </Route>
+      </Routes>
     </div>
   )
 }
+/*************  ✨ Codeium Command ⭐  *************/
+/**
+ * AppWrapper component wraps the App component with a Router to enable routing.
+ * It provides the routing context needed for the application to handle navigation.
+ */
+
+/******  96db7757-432d-46e3-9db1-487aa880af2b  *******/
 function AppWrapper() {
   return (
     <Router>
